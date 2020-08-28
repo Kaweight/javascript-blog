51a5c40c96d704501e8734c3a5f66783acc90954
Kamil Mazelanik <kamilmaz9206@gmail.com>
2020-08-28 09:56:46 +0200

    remove old function generateTitleLinks and create new + fix show links in left column

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
 function titleClickHandler(event) {
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
 
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -30,8 +33,39 @@ function titleClickHandler(event) {
     targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+// const links = document.querySelectorAll('.titles a');
+// console.log(links);
+// for (let link of links) {
+//     link.addEventListener('click', titleClickHandler);
+// }
 
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(CustomSelector = '') {
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector + CustomSelector);
+    let html = '';
+    for (let article of articles) {
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+        /* find the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        /* insert link into titleList */
+        html = html + linkHTML;
+        // console.log(html);
+    }
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
 }
+
+generateTitleLinks();
